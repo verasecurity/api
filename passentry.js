5a533db77949fddf42427bda193a7e8d720213c3
verasecurity <discordkloud@gmail.com>
2026-02-05 14:50:58 +0000

    Update passentry.js

--- passentry.js
+++ passentry.js
@@ -1,1 +1,19 @@
+export default function handler(req, res) {
+  const now = new Date().toISOString();
 
+  res.status(200).json({
+    serialNumber: req.query.serialNumber,
+    barcode: {
+      format: "PKBarcodeFormatQR",
+      message: `USER123|${now}`,
+      messageEncoding: "iso-8859-1"
+    },
+    secondaryFields: [
+      {
+        key: "last_update",
+        label: "Last Refresh",
+        value: now
+      }
+    ]
+  });
+}
